3c49b8eef0329024893dfa939f44c355bc5cf197
Sunil Munavalli <sunil104.munavalli@gmail.com>
2014-09-27 15:51:56 -0500

    First Change

--- Project1/src/aos/com/StartHere.java
+++ Project1/src/aos/com/StartHere.java
@@ -3,6 +3,6 @@ package aos.com;
 public class StartHere {
 	public static void main(String[] args) {
 		System.out.println("Am the starting point");
-		
+		System.out.println("Adding a line for testing");
 	}
 }
